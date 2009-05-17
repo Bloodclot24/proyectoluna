4eb4afa0b9accd6374522f6c9d0ecf73dab493cf
edgardo.skl <edgardo.skl@0b8689fa-25fd-11de-870d-bbf10152ea7e>
2009-05-17 20:16:29 +0000

    Merge funcionando. Faltan modificaciones menores pero ya funciona.

--- trunk/merge.c
+++ trunk/merge.c
@@ -1,63 +1,63 @@
 #include "merge.h"
 
 void merge(Tparticiones *Gparticion){
-  int cantidad = Gparticion ->cantidad;
-  Tarch *archivo[cantidad];
+  int n=3; // n = cantidad de archivos a abrir.
   int i;
-  int posMenor =0;
-  char*  menor;
+  int posMenor=0;
+  unsigned int nEof=0;
+  char* menor;
+  char* Eof;
   char** palabraAComparar;
-  int n=4; // n = cantidad de archivos a abrir.
-  char* Eof= (char*) malloc(sizeof(char)*n);
-  unsigned int nEof= 0;
-  Tarch *archFinal= Fopen("archivoFinal", "w");
-
+  Tarch *archivo[n];
+  
+  Eof = (char*) malloc(sizeof(char)*n);
   palabraAComparar = (char**)malloc(n*sizeof(char*));
+  Tarch *archFinal= Fopen("archivoFinal", "w");
   
-  for(i=0; i<cantidad; i++)
-  		archivo[i]=  obtenerParticion(Gparticion);
+  while((Gparticion->cantidad)>0 ){
+  if((Gparticion->cantidad) < n) n=Gparticion->cantidad;
   
-  for(i=0; i<n; i++) {
-  	FreadLn(archivo[i], (void**)&palabraAComparar[i]);
-  	Eof[i]= '0';
-  }
-  
-  /*for(i=0; i<n; i++) {
-  	printf("%s\n", palabraAComparar[i]);
-  }
-  
-  printf("Pos menor %d\n", posMenor );
-  */
-  while(nEof <= n+1) {
-	  menor= palabraAComparar[posMenor];
-	  printf("El menor: %s\n", menor);
-	  for(i=0; i<n ; i++){
-		if(Eof[i] != '1') {
-			if(strcmp (menor, palabraAComparar[i]) > 0){
-		    	menor = palabraAComparar[i];
-		    	posMenor = i;
-		    	printf("El menor nuevo: %s\n", menor);
-		    }
-		}
-	  }
-		Fwrite(archFinal, menor, strlen(menor));
-		FreadLn(archivo[posMenor], (void**)&palabraAComparar[posMenor]);	  
+  //Obtengo n particiones del grupo de particiones. 
+  for(i=0; i<n; i++) archivo[i] = obtenerParticion(Gparticion);  
 		
-		if(feof(archivo[posMenor]->fd)) {
-		 	Eof[posMenor]= '1';
-		  	nEof++;
-		  	printf("termino %d\n", posMenor);
-		  	
-		  	//posMenor= 1;
-		  }
-		 	
-	 
-		  
-	  }
- 
+  //Leo la primer palabra de cada particion de n cantidad de archivos.
+  //inicializo el vector Eof[] con flags para saber si el archivo termino.
+  for(i=0; i<n; i++) {
+  	if(!feof(archivo[i]->fd)){ 
+  		FreadLn(archivo[i], (void**)&palabraAComparar[i]);
+   		Eof[i]= '0';
+  	}
+  	else{ 
+  		Eof[i]= '1';
+  		nEof ++;
+  	}
+  }
+  	
+  while(nEof < n) {
+ 	if(Eof[posMenor] == '1'){
+   		posMenor = 0;
+   		while(Eof[posMenor]=='1') posMenor ++;
+ 	}		
+ 	menor = palabraAComparar[posMenor];
+    for(i=0; i<n ; i++){
+    	if(Eof[i] != '1') {
+			if(strcmp (menor, palabraAComparar[i]) > 0){
+	  			menor = palabraAComparar[i];
+	  			posMenor = i;
+			}
+     	 }
+    }
+  	Fwrite(archFinal, menor, strlen(menor));
+   
+    if(feof(archivo[posMenor]->fd)) {
+    	Eof[posMenor]= '1';
+      	nEof++;
+    }
+    else {
+    	FreadLn(archivo[posMenor], (void**)&palabraAComparar[posMenor]);	  
+    }
+  }//fin del while de las n particiones.
+  nEof=0;
+  }//fin del while de todo el grupo de particiones.
   
-
-    
 }
-
-

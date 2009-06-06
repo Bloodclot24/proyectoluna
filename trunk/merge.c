8dae0726066a49ec04712dca5343613c60f7023b
vero13@gmail.com <vero13@gmail.com@0b8689fa-25fd-11de-870d-bbf10152ea7e>
2009-06-06 18:45:52 +0000

    Subo modificacion al merge para el lexico, ahora saca las palabras que estan repetidas.

--- trunk/merge.c
+++ trunk/merge.c
@@ -1,67 +1,5 @@
 #include "merge.h"
 
-void merge(Tparticiones *Gparticion){
-  int n=3; // n = cantidad de archivos a abrir.
-  int i;
-  int posMenor=0;
-  unsigned int nEof=0;
-  char* menor;
-  char* Eof;
-  char** palabraAComparar;
-  Tarch *archivo[n];
-  
-  Eof = (char*) malloc(sizeof(char)*n);
-  palabraAComparar = (char**)malloc(n*sizeof(char*));
-  Tarch *archFinal= Fopen("archivoFinal", "w");
-  
-  while((Gparticion->cantidad)>0 ){
-  if((Gparticion->cantidad) < n) n=Gparticion->cantidad;
-  
-  //Obtengo n particiones del grupo de particiones. 
-  for(i=0; i<n; i++) archivo[i] = obtenerParticion(Gparticion);  
-		
-  //Leo la primer palabra de cada particion de n cantidad de archivos.
-  //inicializo el vector Eof[] con flags para saber si el archivo termino.
-  for(i=0; i<n; i++) {
-  	if(!Feof(archivo[i])){ 
-  		FreadLn(archivo[i], (void**)&palabraAComparar[i]);
-   		Eof[i]= '0';
-  	}
-  	else{ 
-  		Eof[i]= '1';
-  		nEof ++;
-  	}
-  }
-  	
-  while(nEof < n) {
- 	if(Eof[posMenor] == '1'){
-   		posMenor = 0;
-   		while(Eof[posMenor]=='1') posMenor ++;
- 	}		
- 	menor = palabraAComparar[posMenor];
-    for(i=0; i<n ; i++){
-    	if(Eof[i] != '1') {
-			if(strcmp (menor, palabraAComparar[i]) > 0){
-	  			menor = palabraAComparar[i];
-	  			posMenor = i;
-			}
-     	 }
-    }
-  	Fwrite(archFinal, menor, strlen(menor));
-   
-    if(Feof(archivo[posMenor])) {
-    	Eof[posMenor]= '1';
-      	nEof++;
-    }
-    else {
-    	FreadLn(archivo[posMenor], (void**)&palabraAComparar[posMenor]);	  
-    }
-  }//fin del while de las n particiones.
-  nEof=0;
-  }//fin del while de todo el grupo de particiones.
-  //Fclose(archFinal);
-}
-
 
 Tarch* merge2(Tparticiones *Gparticion, int cantidad){
      if(cantidad <= 1)
@@ -69,11 +7,13 @@ Tarch* merge2(Tparticiones *Gparticion, int cantidad){
 
      char* nuevoNombre;
      char numero[6];
+	
      nuevoNombre = malloc(strlen(Gparticion->nombre)+5);
      strcpy(nuevoNombre, Gparticion->nombre);
      strcat(nuevoNombre, "_bis");
      Tparticiones* nuevoGrupo = crearParticiones(nuevoNombre);
      free(nuevoNombre);
+     int frec=1;
      
      int n=cantidad; // n = cantidad de archivos a abrir.
      int i;
@@ -83,9 +23,11 @@ Tarch* merge2(Tparticiones *Gparticion, int cantidad){
      char* Eof = (char*) malloc(n*sizeof(char));
      char** palabraAComparar = (char**)malloc(n*sizeof(char*));
      Tarch **archivo = (Tarch**)malloc(n*sizeof(Tarch*));
-
+	 char *auxmenor="\0";
      int contador=0;
-
+	 char *frecchar = (char*)malloc(sizeof(int));
+	 
+	 	
      Tarch *archFinal;
 
      for(i=0;i<n;i++) palabraAComparar[i] = NULL;
@@ -118,30 +60,36 @@ Tarch* merge2(Tparticiones *Gparticion, int cantidad){
 	       }
 	  }
 	  
+	  
+	  
 	  while(nEof < n){
-	       if(Eof[posMenor] == '1'){
-		    posMenor = 0;
-		    while(Eof[posMenor]=='1') posMenor ++;
-	       }		
-	       menor = palabraAComparar[posMenor];
-	       for(i=0; i<n ; i++){
-		    if(Eof[i] != '1') {
-			 if(strcmp(menor, palabraAComparar[i]) > 0){
-			      menor = palabraAComparar[i];
-			      posMenor = i;
-			 }
+	  	if(Eof[posMenor] == '1'){
+			posMenor = 0;
+		   	while(Eof[posMenor]=='1') posMenor ++;
+	    }		
+	    
+	    menor = palabraAComparar[posMenor];
+	    for(i=0; i<n ; i++){
+			if(Eof[i] != '1') {
+				if(strcmp(menor, palabraAComparar[i]) > 0){
+			    	menor = palabraAComparar[i];
+			    	posMenor = i;
+			 	}
 		    }
-	       }
-	       Fwrite(archFinal, menor, strlen(menor));
-	       free(menor);
-	       
-	       if(Feof(archivo[posMenor])){
-		    Eof[posMenor]= '1';
+	    }
+       if(strcmp(menor, auxmenor)!=0 ){
+			Fwrite(archFinal, menor, strlen(menor));
+       }
+      	auxmenor = menor;
+        //free(menor);
+	   
+	   if(Feof(archivo[posMenor])){
+	   		Eof[posMenor]= '1';
 		    /* llego a EOF, el archivo no sirve mas, lo borro*/
-		    Funlink(archivo[posMenor]); 
+		   	Funlink(archivo[posMenor]); 
 		    nEof++;
-	       }
-	       else {
+	      }
+	   else {
 		    FreadLn(archivo[posMenor], (void**)&palabraAComparar[posMenor]);
 	       }
 	  }//fin del while de las n particiones.
